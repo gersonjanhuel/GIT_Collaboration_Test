20137d2ff284c7d66f490aa32b939869e9f4b507
Dian DInihari | Tia <55896324+ddhyuliani@users.noreply.github.com>
2023-05-16 09:56:08 +0700

    nambah 1 file detail

---
+++ GIT_Collaboration_Test/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  GIT_Collaboration_Test
+//
+//  Created by Dini on 16/05/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        List {
+                Text("Dini")
+                Text("Gerson")
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
