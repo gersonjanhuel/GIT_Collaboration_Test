0c5946beb2c525828c3d29126c380e7661e8ffc7
Gerson Janhuel <gersonjanhuel@gmail.com>
2023-05-17 14:38:23 +0700

    complete sample project

--- GIT_Collaboration_Test/DetailView.swift
+++ GIT_Collaboration_Test/DetailView.swift
@@ -8,12 +8,53 @@
 import SwiftUI
 
 struct DetailView: View {
+    @State private var isLoved: Bool = false
+    
     var body: some View {
-        List {
-            Text("Dini")
-            Text("Gerson")
-            Text("See all animals")
+        VStack() {
+            Rectangle()
+                .frame(width: 90, height: 80)
+                .foregroundColor(.blue)
+                .padding(.top, 30)
+                .padding(.bottom, 20)
+            
+            Rectangle()
+                .frame(width: 150, height: 20)
+                .padding(.bottom, 50)
+            
+            Rectangle()
+                .frame(height: 20)
+                .padding(.horizontal, 40)
+            Rectangle()
+                .frame(height: 20)
+                .padding(.horizontal, 40)
+            Rectangle()
+                .frame(height: 20)
+                .padding(.horizontal, 40)
+            Rectangle()
+                .frame(height: 20)
+                .padding(.horizontal, 40)
+            Rectangle()
+                .frame(height: 20)
+                .padding(.horizontal, 40)
+            Rectangle()
+                .frame(height: 20)
+                .padding(.horizontal, 40)
+            
+            Spacer()
         }
+        .foregroundColor(.orange)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    isLoved.toggle()
+                } label: {
+                    Image(systemName: isLoved ? "heart.fill" : "heart")
+                }
+
+            }
+        }
+        
     }
 }
 
